aca70d5ee1d18dd5798d3650bcb587e1c0afd60a
amineaouini-notbot <aminaouini565@gmail.com>
2024-02-20 16:02:26 +0100

    init

---
+++ src/main/java/com/hello/writeAndRead/WriteAndReadApplication.java
@@ -0,0 +1,13 @@
+package com.hello.writeAndRead;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WriteAndReadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WriteAndReadApplication.class, args);
+	}
+
+}
